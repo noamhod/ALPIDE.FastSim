a4796d1d55e7fc46243d62b8a04b2eedfbf4f796
Noam Hod <nhod@wipp-an02.weizmann.ac.il>
2020-09-12 17:34:27 +0300

    updated batch mode

--- Root/job.sh
+++ Root/job.sh
@@ -1,3 +1,20 @@
+echo "You are here: $PWD"
+export BASEBPTH=/srv01/agrp/nhod/ALPIDE.FastSim/Root
+cd $BASEBPTH
+echo "Now you are here: $PWD"
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
+cd $ATLAS_LOCAL_ROOT_BASE/user
+echo "Now you are here: $PWD"
+source atlasLocalSetup.sh
+setupATLAS
+lsetup "root 6.16.00-x86_64-centos7-gcc8-opt"
+export STORAGEDIR=/storage/agrp/nhod/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
+echo "STORAGEDIR=: $STORAGEDIR"
+cd $BASEBPTH
+echo "Now you are here: $PWD"
+ls -lrth
 ./digi -proc=bppp_bkg -evnt=$1
 ./digi -proc=bppp     -evnt=$1
 ./reco -proc=bppp     -evnt=$1
--- Root/send_qsub.py
+++ Root/send_qsub.py
@@ -3,12 +3,20 @@ import os, sys
 import subprocess
 from subprocess import call
 
-nevents = 10
+nevents = 980
 
 for ievnt in range(nevents):
    sievnt = str(ievnt)
-   # command = "source job.sh "+str(ievnt)
-   command = 'qsub job.sh -- '+sievnt+'  -q N  -o $STORAGEDIR/log_'+sievnt+'.out -e $STORAGEDIR/log_'+sievnt+'.err'
+   p = subprocess.Popen("mkdir -p $STORAGEDIR/logs", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+   out, err = p.communicate()
+   p = subprocess.Popen("rm -f $STORAGEDIR/logs/*", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+   out, err = p.communicate()
+   command = 'qsub -F "'+sievnt+'" job.sh -q N  -o $STORAGEDIR/logs/log_'+sievnt+'.out -e $STORAGEDIR/logs/log_'+sievnt+'.err'
    print command
    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
    out, err = p.communicate()
+   print "out=",out
+   if(err!=""): print "err=",err
+
+print "Check with: `qstat -u user`"
+print "List logs: `ls -lrth $STORAGEDIR/logs/`"
