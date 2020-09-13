c324bcf91586e32eef7f9d677cebddf7725c8be3
Noam Tal Hod <noam.hod@cern.ch>
2020-09-13 14:06:58 +0300

    updated send batch script

--- Root/send_qsub.py
+++ Root/send_qsub.py
@@ -2,14 +2,25 @@
 import os, sys
 import subprocess
 from subprocess import call
+import argparse
+parser = argparse.ArgumentParser(description='send_qsub.py...')
+parser.add_argument('-p', metavar='process', required=True,  help='physics process [trident or bppp]')
+parser.add_argument('-c', metavar='clean',   required=True,  help='y/n')
+parser.add_argument('-r', metavar='resubmit',required=False, help='"[1,2,6,99,134,...]"')
+argus = parser.parse_args()
+proc  = argus.p
+clean = (argus.c=="y")
+resubmit = (argus.r!=None)
 
-clean = False
+ievents = []
+if(not resubmit):
+   nevents = 980
+   for ievnt in range(nevents): ievents.append(ievnt)
+else:
+   sievents = (argus.r).replace("[","").replace("]","").split(",")
+   for sievent in sievents:
+      ievents.append(int(sievent))
 
-#ievents = []
-#nevents = 980
-#for ievnt in range(nevents): ievents.append(ievnt)
-
-ievents = [450]
 
 p = subprocess.Popen("mkdir -p $STORAGEDIR/logs", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 out, err = p.communicate()
@@ -21,13 +32,10 @@ out, err = p.communicate()
 if(clean):
    p = subprocess.Popen("rm -f $STORAGEDIR/logs/*", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
    out, err = p.communicate()
-
    p = subprocess.Popen("rm -f $STORAGEDIR/data/root/dig/*.root", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
    out, err = p.communicate()
-
    p = subprocess.Popen("rm -f $STORAGEDIR/data/root/rec/*.root", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
    out, err = p.communicate()
-
 
 for ievnt in ievents:
    sievnt = str(ievnt)
