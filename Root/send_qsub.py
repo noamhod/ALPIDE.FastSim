9eaa0eeccc1883288fd71a0959062563dc0531bf
Noam Tal Hod <noam.com@gmail.com>
2020-09-13 13:32:17 +0300

    up

--- Root/send_qsub.py
+++ Root/send_qsub.py
@@ -3,24 +3,42 @@ import os, sys
 import subprocess
 from subprocess import call
 
+clean = False
+
 #ievents = []
 #nevents = 980
 #for ievnt in range(nevents): ievents.append(ievnt)
 
-ievents = [99, 199, 299, 399, 499, 599, 699, 799, 899]
+ievents = [450]
+
+p = subprocess.Popen("mkdir -p $STORAGEDIR/logs", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+out, err = p.communicate()
+p = subprocess.Popen("mkdir -p $STORAGEDIR/data/root/dig", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+out, err = p.communicate()
+p = subprocess.Popen("mkdir -p $STORAGEDIR/data/root/rec", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+out, err = p.communicate()
+
+if(clean):
+   p = subprocess.Popen("rm -f $STORAGEDIR/logs/*", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+   out, err = p.communicate()
+
+   p = subprocess.Popen("rm -f $STORAGEDIR/data/root/dig/*.root", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+   out, err = p.communicate()
+
+   p = subprocess.Popen("rm -f $STORAGEDIR/data/root/rec/*.root", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+   out, err = p.communicate()
+
 
 for ievnt in ievents:
    sievnt = str(ievnt)
-   p = subprocess.Popen("mkdir -p $STORAGEDIR/logs", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-   out, err = p.communicate()
-   p = subprocess.Popen("rm -f $STORAGEDIR/logs/*", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-   out, err = p.communicate()
    command = 'qsub -F "'+sievnt+'" job.sh -q N  -o $STORAGEDIR/logs/log_'+sievnt+'.out -e $STORAGEDIR/logs/log_'+sievnt+'.err'
    print command
    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
    out, err = p.communicate()
-   print "out=",out.replace("\n","")
-   if(err!=""): print "err=",err.replace("\n","")
+   print("out=",out.replace("\n",""))
+   if(err!=""): print("err=",err.replace("\n",""))
 
-print "Check with: `qstat -u user`"
-print "List logs: `ls -lrth $STORAGEDIR/logs/`"
+
+print("Check with:        qstat -u user")
+print("List logs with:    ls -lrth $STORAGEDIR/logs/")
+print("Check output with: python check_qsub.py")
