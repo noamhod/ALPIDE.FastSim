72dd6464e3bdcaea4da032d64458c6a88c065944
Noam Tal Hod <noam.hod@cern.ch>
2020-09-28 12:03:09 +0300

    added job_truth

--- python/job_truth.sh
+++ python/job_truth.sh
@@ -12,6 +12,7 @@ echo storage dir is set: $STORAGEDIR
 echo ROOTSYS=$ROOTSYS
 echo PYTHONPATH=$PYTHONPATH
 which python
+which root
 
 echo submitting stdhep2root:
 python3.7 stdhep2root.py   -p $1 -d $2
